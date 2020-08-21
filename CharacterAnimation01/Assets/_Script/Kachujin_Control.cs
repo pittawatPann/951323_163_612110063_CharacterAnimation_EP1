d6c6ae9b42545bbcb5bd5f3cacc887269df3b840
DESKTOP-325D3AA\Pittawat <pittawat_pan@cmu.ac.th>
2020-08-21 15:21:33 +0700

    Push UnityProject AnimationChar

---
+++ CharacterAnimation01/Assets/_Script/Kachujin_Control.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kachujin_Control : MonoBehaviour
+{
+    Animator _animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            _animator.SetTrigger("Kick");
+        }
+        if(Input.GetKeyDown(KeyCode.X))
+        {
+            _animator.SetTrigger("Punch");
+        }
+        if(Input.GetKeyDown(KeyCode.LeftAlt))
+        {
+            _animator.SetBool("Dancing", true);
+        }
+        else if(Input.GetKeyUp(KeyCode.LeftAlt))
+        {
+            _animator.SetBool("Dancing", false);
+        }
+    }
+}
